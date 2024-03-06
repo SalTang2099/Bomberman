5b5fca0a0ee7574bc846c0e77736e7a10c67ae4b
Rikaero <ritvikdeepika@gmail.com>
2024-03-06 17:03:35 -0500

    fixed merge conflict

--- Assets/Scripts/startgame.cs
+++ Assets/Scripts/startgame.cs
@@ -7,11 +7,7 @@ public class startgame : MonoBehaviour
 {
     public void Playgame()
     {
-<<<<<<< HEAD
         SceneManager.LoadScene("Bomberman");
-=======
-        SceneManager.LoadScene("BombermanTest");
->>>>>>> 2a2a7619e7ce2e4f747d9a2f559b4c90a25db518
     }
 
     public void PlayAILevel()
